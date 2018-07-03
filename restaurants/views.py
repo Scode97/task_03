4728b66b8a1460960a23f5771e960eae7d952e86
scode97 <sehameldeen1997@gmail.com>
2018-07-03 18:27:24 +0300

    done with the code

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,13 +7,35 @@ def restaurant_list(request):
 
     context = {
 
+    	"my_list": [
+            {
+                "restaurant_name":"Elevation Burger",
+                "food_type":"Organics",
+            },
+            {
+                "restaurant_name":"Burger King",
+                "food_type":"Fast food",
+            },
+            {
+                "restaurant_name":"Pick",
+                "food_type":"Healthy snacks",
+            },
+        ],
+
     }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
-    context = {
+    context = {  
+
+    	"my_object": 
+            {
+                "restaurant_name":"Pick",
+                "food_type":"Healthy snacks"
+            }
+            
 
     }
     return render(request, 'detail.html', context)
